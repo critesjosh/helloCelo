2f26ed113b2256dd228b99162dad673755329464
Josh <critesjosh@gmail.com>
2020-04-28 21:37:21 -0400

    Update helloCello.js

--- helloCello.js
+++ helloCello.js
@@ -1,8 +1,52 @@
-// add package imports and setup up here
+//
+// Add package imports and setup here
+//
 
-async function awaitWrapper(){
-    // add async/await code in this function
+// 1. Import ContractKit
 
+// 2. Init a new kit, connected to the alfajores testnet
+
+//
+// Read Accounts
+//
+
+async function readAccount(){
+    // 3. Get the Gold Token contract
+    // 4. Address to look up
+    // 5. Get Gold Token Balance
+    // Print balance
 }
 
-awaitWrapper()
+//
+// Create an Account
+//
+
+// 6. Import the getAccount function
+
+async function createAccount(){
+    // 7. Get your account    
+    // 8. Get the Gold Token contract wrapper    
+    // 9. Get your Celo Gold balance
+    // Print your account info
+}
+
+//
+// Send Gold
+//
+
+async function send(){
+    // 10.  Get your account
+    // 11. Add your account to ContractKit to sign transactions
+    // 12. Specify recipient Address
+    // 13. Specify an amount to send
+    // 14. Get the Gold Token contract wrapper    
+    // 15. Transfer gold from your account to anAddress
+    // 16. Wait for the transaction to be processed
+    // 17. Print receipt
+    // 18. Get your new balance
+    // 19. Print new balance
+}
+
+readAccount()
+createAccount()
+send()
