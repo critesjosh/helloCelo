2c2c932a9aa7d2e5c56d267ac58de02f8713d6af
Josh <critesjosh@gmail.com>
2021-01-15 13:47:58 -0500

    update gold references to CELO

--- helloCello.js
+++ helloCello.js
@@ -11,9 +11,9 @@
 //
 
 async function readAccount(){
-    // 3. Get the Gold Token contract
+    // 3. Get the Gold Token (CELO) contract
     // 4. Address to look up
-    // 5. Get Gold Token Balance
+    // 5. Get Gold Token (CELO) Balance
     // Print balance
 }
 
@@ -26,7 +26,7 @@ async function readAccount(){
 async function createAccount(){
     // 7. Get your account    
     // 8. Get the Gold Token contract wrapper    
-    // 9. Get your Celo Gold balance
+    // 9. Get your CELO balance
     // Print your account info
 }
 
@@ -39,8 +39,8 @@ async function send(){
     // 11. Add your account to ContractKit to sign transactions
     // 12. Specify recipient Address
     // 13. Specify an amount to send
-    // 14. Get the Gold Token contract wrapper    
-    // 15. Transfer gold from your account to anAddress
+    // 14. Get the Gold Token (CELO) contract wrapper    
+    // 15. Transfer CELO from your account to anAddress
     // 16. Wait for the transaction to be processed
     // 17. Print receipt
     // 18. Get your new balance
